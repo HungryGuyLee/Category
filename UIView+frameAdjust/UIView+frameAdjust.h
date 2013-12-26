7681d6ba9cffb124bbebbe93bff745a37223ba9c
NJ惺忪成性 <leeambition@gmail.com>
2013-12-26 17:51:58 +0800

    init

---
+++ UIView+frameAdjust/UIView+frameAdjust.h
@@ -0,0 +1,40 @@
+//
+//  UIView+frameAdjust.h
+//
+//  Created by Lee on 13-12-25.
+//  Copyright (c) 2013年 Lee . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIView (frameAdjust)
+
+- (CGPoint)origin;
+- (void)setOrigin:(CGPoint) point;
+
+- (CGSize)size;
+- (void)setSize:(CGSize) size;
+
+- (CGFloat)x;
+- (void)setX:(CGFloat)x;
+
+- (CGFloat)y;
+- (void)setY:(CGFloat)y;
+
+- (CGFloat)height;
+- (void)setHeight:(CGFloat)height;
+
+- (CGFloat)width;
+- (void)setWidth:(CGFloat)width;
+
+- (CGFloat)tail;
+- (void)setTail:(CGFloat)tail;
+
+- (CGFloat)bottom;
+- (void)setBottom:(CGFloat)bottom;
+
+- (CGFloat)right;
+- (void)setRight:(CGFloat)right;
+
+
+@end
